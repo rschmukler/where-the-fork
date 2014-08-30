c0a490c7a3af8ab42b349c0f12de48dbf4fdca0c
Ryan Schmukler <rschmukler@gmail.com>
2014-08-30 15:02:19 -0500

    fix routing, add search page files

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 var koa = require('koa'),
     mount = require('koa-mount'),
     Router = require('koa-router'),
-    serveStatic = require('koa-static');
+    serveStatic = require('koa-static'),
+    readFile = require('co-fs').readFile;
 
 var app = module.exports = koa();
+
+app.use(Router(app));
 
 if(process.env.NODE_ENV != 'production') {
   app.use(serveStatic('public/'));
@@ -19,6 +22,12 @@ apis.forEach(function(api) {
 });
 
 app.use(mount('/api', endpoint.middleware()));
+
+app.get(/.*/, function*(next) {
+  yield next;
+  if(!this.body) this.body = yield readFile('public/index.html', 'utf8');
+});
+
 
 console.log("Launching application on port 3000");
 app.listen(3000);
